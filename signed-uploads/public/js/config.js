08d4b13308f760c44230a45afe8261a26750c51f
Dmytro Cisar <dmytro.cisar@gmail.com>
2021-08-30 01:23:40 +0300

    via dotenv

--- signed-uploads/public/js/config.js
+++ signed-uploads/public/js/config.js
@@ -1,13 +1,12 @@
+require('dotenv').config()
+
 const cloudinary = require('cloudinary').v2;
 
-// Configure your cloud name, API key and API secret:
-
 const myconfig = cloudinary.config({
-  cloud_name: '',
-  api_key: '',
-  api_secret: '',
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
   secure: true
 });
-
 
 exports.myconfig = myconfig;
